325102c7e4d3d94686b6ce6a5f863889737d856b
dongchijoseph <huyblogging@gmail.com>
2021-04-09 11:26:41 +0700

    done coding

--- script.js
+++ script.js
@@ -1,73 +1,69 @@
-let playOption = ["rock", "paper", "scissors"];
+const finalResult = document.querySelector("#finalResult");
+const displayScore = document.querySelector("#displayScore");
+const buttons = document.querySelectorAll("button");
+let playerScore = 0;
+let computerScore = 0;
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    const container = document.querySelector("#container");
+    const result = document.createElement("p");
+    let playRoundResult = playRound(button.id, computerPlay());
+    result.textContent = playRoundResult;
+    container.appendChild(result);
+    console.log(playRoundResult);
+
+    calcScore(playRoundResult);
+
+    displayScore.textContent = `YOU: ${playerScore} / COMPUTER: ${computerScore}`;
+
+    if (playerScore === 5) {
+      finalResult.textContent = "You Won";
+    } else if (computerScore === 5) {
+      finalResult.textContent = "You losed";
+    }
+  });
+});
+
+function calcScore(score) {
+  if (playerScore < 5 && computerScore < 5) {
+    if (score.indexOf("Win") === 4) {
+      playerScore++;
+    } else if (score.indexOf("Lose") === 4) {
+      computerScore++;
+    }
+  }
+}
 
 function computerPlay() {
-    return playOption[Math.floor(Math.random() * 3)];
+  const playOption = ["rock", "paper", "scissors"];
+  return playOption[Math.floor(Math.random() * 3)];
 }
 
 function toCapital(str) {
-    return str.charAt(0).toUpperCase() + str.slice(1);
+  return str.charAt(0).toUpperCase() + str.slice(1);
 }
 
 function playRound(playerSelection, computerSelection) {
-    let playerSelectionLowerCase = playerSelection.toLowerCase();
-    
-    switch(true) {
-        case playerSelectionLowerCase === "rock" && computerSelection === "paper":
-        case playerSelectionLowerCase === "paper" && computerSelection === "scissors":
-        case playerSelectionLowerCase === "scissors" && computerSelection === "rock":
-            return `You Lose! ${toCapital(computerSelection)} beats ${toCapital(playerSelectionLowerCase)}`;
-            break;
-        
-        case playerSelectionLowerCase === "paper" && computerSelection === "rock":
-        case playerSelectionLowerCase === "scissors" && computerSelection === "paper":
-        case playerSelectionLowerCase === "rock" && computerSelection === "scissors":
-        return `You Win! ${toCapital(playerSelectionLowerCase)} beats ${toCapital(computerSelection)}`;
+  if (playerScore < 5 && computerScore < 5) {
+    switch (true) {
+      case playerSelection === "rock" && computerSelection === "paper":
+      case playerSelection === "paper" && computerSelection === "scissors":
+      case playerSelection === "scissors" && computerSelection === "rock":
+        return `You Lose! ${toCapital(computerSelection)} beats ${toCapital(
+          playerSelection
+        )}`;
         break;
 
-        case playerSelectionLowerCase !== "rock" && playerSelectionLowerCase !== "paper" && playerSelectionLowerCase !== "scissors":
-            return `You have inputed: \"${playerSelection}\". You need to input rock, paper, or scissors.`;
-            break;
-        
-        default:
-            return "Draw! Let's play more";
-    }
+      case playerSelection === "paper" && computerSelection === "rock":
+      case playerSelection === "scissors" && computerSelection === "paper":
+      case playerSelection === "rock" && computerSelection === "scissors":
+        return `You Win! ${toCapital(playerSelection)} beats ${toCapital(
+          computerSelection
+        )}`;
+        break;
 
+      default:
+        return "Draw! Let's play more";
+    }
+  }
 }
-
-function game() {
-    let playerScore = 0;
-    let computerScore = 0;
-    
-    for (let i = 0; i < 5; i++) {
-        let userInput = window.prompt("Welcome, please input rock, paper, or scissors", "rock");
-        let result = playRound(userInput, computerPlay());
-        if (result.indexOf("Win") === 4) {
-            playerScore++;
-            console.log(result);
-        } else if (result.indexOf("Lose") === 4) {
-            computerScore++;
-            console.log(result);
-        } else if (result.indexOf("!") === 4) {
-            console.log(result);
-        } else {
-            console.log(result);
-        }
-        console.log(`You: ${playerScore} Computer: ${computerScore}`);
-    }
-
-    switch(true) {
-        case playerScore > computerScore:
-            console.log(`Final result: You win\nYou: ${playerScore} Computer: ${computerScore}`);
-            break;
-        case playerScore < computerScore:
-            console.log(`Final result: You lose\nYou: ${playerScore} Computer: ${computerScore}`);
-            break;
-        default:
-            console.log("Final result: Draw!");
-    }
-    
-
-}
-
-game();
-
